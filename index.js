eee312a0fc2b1186ff194e59ef9837bb955e4afa
BarakaIsaac <isaac.mwandau2@gmail.com>
2022-12-04 12:44:47 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+random;
+
+let num3 = 34;
+let num4 = 6;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(4, 6, 10, 13, 15, 17, 20);
+max;
